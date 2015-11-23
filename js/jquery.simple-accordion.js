35239d78acd498cfa04f92b3cae0a7955ac60e3a
Gary Cuga-Moylan <gary@telegraphicsinc.com>
2015-11-22 18:00:40 -0600

    Aligning project with others in the simple series. Added scss file

--- js/jquery.simple-accordion.js
+++ js/jquery.simple-accordion.js
@@ -1,9 +1,12 @@
 (function IIFE($) {
 	$.fn.simpleAccordion = function() {
 		this.on("click", ".accordion__control", function() {
+			// Toggle the panel next to the item that was clicked
 			$(this).toggleClass("accordion__control--active");
 			$(this).next().toggle();
 		});
+
+		// Return jQuery object for method chaining
 		return this;
 	}
 })(jQuery);
